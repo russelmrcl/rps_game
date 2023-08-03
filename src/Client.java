18f3d62253823ba9035fcdab67e2ade139a91c21
Russel Marcelo <russelmarcelo1509@gmail.com>
2023-08-03 02:28:54 +0200

    Initial commit

--- src/Client.java
+++ src/Client.java
@@ -4,39 +4,53 @@ import java.util.Scanner;
 
 public class Client {
 
+    // Store the player name
     private static String playerName;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         String input;
+
+        // Create a socket connection to the server running on localhost and port 1337
         Socket server = new Socket("localhost", 1337);
 
+        // Set up input and output streams to communicate with the server
         BufferedReader inputUser = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter writer = new PrintWriter(server.getOutputStream(), true);
         BufferedReader inputServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
 
+        // Welcome message and get the player's name
         System.out.println("Welcome to rock paper scissors!");
         System.out.println("Enter your name :");
         playerName = scanner.nextLine();
 
+        // Game loop: continuously prompt the player for their choice until the game is over
         while (true) {
             do {
-                System.out.println("Select: Rock[r], Paper[p] or Scissors[s]");
-                input = inputUser.readLine();
+                // Ask the player to select rock, paper, or scissors
+                System.out.println("Select: [R]ock, [P]aper, or [S]cissors");
+                input = inputUser.readLine().toLowerCase();
 
             } while (!input.equals("r") && !input.equals("p") && !input.equals("s"));
 
+            // Send the player's name and choice to the server
             writer.println(playerName + ": " + input);
-            // Wait for response from server and display it
+
+            // Wait for response from the server and display it
             System.out.println("Wait for response");
             String serverResponse = inputServer.readLine();
             System.out.println("Result: " + serverResponse);
-            // Terminate client if the server response is null (i.e., the server connection is closed)
+
+            // Terminate the client if the server response is null (i.e., the server connection is closed)
             if (serverResponse == null) {
                 break;
             }
+
+            // Exit the loop (end the game) after one round
             break;
         }
+
+        // Close the socket and end the program
         server.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -11,41 +11,50 @@ public class Server {
     private static String playerTwoChoice;
 
     public static void main(String[] args) throws IOException {
+        // Create a server socket on port 1337 to listen for client connections
         ServerSocket chatServer = new ServerSocket(port);
 
+        // Wait for the first client to connect
         System.out.println("Waiting for first client...");
         clientOne = chatServer.accept();
-        System.out.println("first client is connected!");
+        System.out.println("First client is connected!");
 
+        // Wait for the second client to connect
         System.out.println("Waiting for second client...");
         clientTwo = chatServer.accept();
-        System.out.println("second client is connected!");
+        System.out.println("Second client is connected!");
 
+        // Handle communication between the two clients
         handleCommunication();
     }
 
     private static void handleCommunication() throws IOException {
+        // Set up input and output streams for the first client
         BufferedReader input1 = new BufferedReader(new InputStreamReader(clientOne.getInputStream()));
         PrintWriter writer1 = new PrintWriter(clientOne.getOutputStream(), true);
 
+        // Set up input and output streams for the second client
         BufferedReader input2 = new BufferedReader(new InputStreamReader(clientTwo.getInputStream()));
         PrintWriter writer2 = new PrintWriter(clientTwo.getOutputStream(), true);
 
-
         while (true) {
-
+            // Read messages from the first client
             String messageFromClient1 = input1.readLine();
+            // If the message is null, it means the first client disconnected, so exit the loop
             if (messageFromClient1 == null) {
                 break;
             }
             System.out.println("Received from first client: " + messageFromClient1);
 
+            // Read messages from the second client
             String messageFromClient2 = input2.readLine();
+            // If the message is null, it means the second client disconnected, so exit the loop
             if (messageFromClient2 == null) {
                 break;
             }
             System.out.println("Received from second client: " + messageFromClient2);
 
+            // Split the messages to extract player names and choices
             String[] parts1 = messageFromClient1.split(": ");
             String[] parts2 = messageFromClient2.split(": ");
 
@@ -56,24 +65,22 @@ public class Server {
                 playerTwoChoice = parts2[1];
             }
 
+            // Determine the game result and send the appropriate messages to the clients
             if (playerOneChoice.equals(playerTwoChoice)) {
-                writer1.println("it is a draw :|");
-                writer2.println("it is a draw :|");
-            }
-
-            //PlayerOneWins
-            if (playerOneChoice.equals("r") && playerTwoChoice.equals("s")
-                    || playerOneChoice.equals("p") && playerTwoChoice.equals("r")
-                    || playerOneChoice.equals("s") && playerTwoChoice.equals("p")) {
-                writer1.println("you won :)");
+                writer1.println("It's a draw :|");
+                writer2.println("It's a draw :|");
+            } else if ((playerOneChoice.equals("r") && playerTwoChoice.equals("s"))
+                    || (playerOneChoice.equals("p") && playerTwoChoice.equals("r"))
+                    || (playerOneChoice.equals("s") && playerTwoChoice.equals("p"))) {
+                writer1.println("You won :)");
                 writer2.println(playerOneName + " won!" + ", you lost :(");
-                //PlayerTwoWins
             } else {
-                writer2.println("you won :)");
+                writer2.println("You won :)");
                 writer1.println(playerTwoName + " won!" + ", you lost :(");
             }
         }
 
+        // Close the sockets when the game is over
         clientOne.close();
         clientTwo.close();
     }
